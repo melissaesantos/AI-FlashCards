3c78753dc9c28212404724ce3e3f749a43a133be
Melissa Santos <melissasantos2202@gmail.com>
2024-08-14 19:11:18 -0600

    payment setup w stripe

--- api/checkout_sessions/route.js
+++ api/checkout_sessions/route.js
@@ -3,22 +3,37 @@ import Stripe from "stripe";
 
 //we created the stripe object
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const formatAmountForStripe = (amount)=>{
+    return Math.round*100
+}
 export async function POST(req){
-    const params: Stripe.Checkout.SessionCreateParams = {
-        submit_type: 'donate',
+    const params= {
+        submit_type: 'subscription',
         payment_method_types: ['card'],
         line_items: [
           {
-            name: 'Custom amount donation',
-            amount: formatAmountForStripe(amount, CURRENCY),
-            currency: CURRENCY,
-            quantity: 1,
+           price_data: {
+                  currency: 'usd',
+                  product_data: {
+                      name: 'Pro Subscription'
+                  },
+                  unit_amount :formatAmountForStripe(10),
+                  recurring:{
+                    interval: 'month',
+                    interval_count: 1,
+
+                  }
+              },
+
+           quantity: 1,
           },
+          
         ],
         success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
       };
-      const checkoutSession: Stripe.Checkout.Session =
-        await stripe.checkout.sessions.create(params);
-
+      const checkoutSession = await stripe.checkout.sessions.create(params)
+      return NextResponse.json(checkoutSession,{
+        status:200
+      })
 }
